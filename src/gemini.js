f5b25c92e9709526e8203c7790f8ff3f6b2e549a
nxctophile <nxctophile@gmail.com>
2024-07-07 23:39:29 +0530

    Fixed API and upgraded to gemini pro

--- src/gemini.js
+++ src/gemini.js
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
 const generate = async (prompt) => {
   const result = await model.generateContent(prompt);
