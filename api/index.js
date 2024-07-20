0c98b29456e0aa6b35e704bd7fa26a4c4dd9226c
nxctophile <nxctophile@gmail.com>
2024-07-20 23:53:35 +0530

    Added whisper model to the /recognize route and changed the /api route to /generate

--- api/index.js
+++ api/index.js
@@ -1,30 +1,54 @@
-import express from 'express';
-import cors from 'cors';
-import generate from '../src/gemini.js';
+import express from "express";
+import cors from "cors";
+import generate from "../src/gemini.js";
+import recognize from "../src/whisper.js";
 
 const app = express();
 
 app.use(cors());
 
-app.get('/api', async (req, res) => {
+app.get("/generate", async (req, res) => {
   if (req.query.prompt) {
     try {
       const generatedText = await generate(req.query.prompt);
       res.json({
         response: generatedText,
-        status: 200
+        status: 200,
       });
     } catch (error) {
       res.status(500).json({
         Error: "Error generating content",
         status: 500,
-        details: error.message
+        details: error.message,
       });
     }
   } else {
     res.status(404).json({
       Error: "Cannot get this route",
-      status: 404
+      status: 404,
+    });
+  }
+});
+
+app.get("/recognize", async (req, res) => {
+  if (req.query.voicedata) {
+    try {
+      const recognizedText = await recognize(req.query.voicedata);
+      res.json({
+        response: recognizedText,
+        status: 200,
+      });
+    } catch (error) {
+      res.status(500).json({
+        Error: "Error recognizing voice",
+        status: 500,
+        details: error.message,
+      });
+    }
+  } else {
+    res.status(404).json({
+      Error: "Cannot get this route",
+      status: 404,
     });
   }
 });
