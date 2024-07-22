14eb2fd9c127882449cebfd3b8acfc2e2928d0a0
nxctophile <nxctophile@gmail.com>
2024-07-22 21:03:03 +0530

    Added image uploads (testing)

--- api/index.js
+++ api/index.js
@@ -1,16 +1,31 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
 import generate from "../src/gemini.js";
-import recognize from "../src/whisper.js";
 
+const upload = multer({ dest: "uploads/" });
 const app = express();
 
 app.use(cors());
 
-app.get("/generate", async (req, res) => {
-  if (req.query.prompt) {
+app.get("/api", async (req, res) => {
+  if (req.query.prompt && !req.query.image) {
     try {
       const generatedText = await generate(req.query.prompt);
+      res.json({
+        response: generatedText,
+        status: 200,
+      });
+    } catch (error) {
+      res.status(500).json({
+        Error: "Error generating content",
+        status: 500,
+        details: error.message,
+      });
+    }
+  } else if (req.query.prompt && req.query.image) {
+    try {
+      const generatedText = await generate(req.query.prompt, req.query.image);
       res.json({
         response: generatedText,
         status: 200,
@@ -30,26 +45,11 @@ app.get("/generate", async (req, res) => {
   }
 });
 
-app.get("/recognize", async (req, res) => {
-  if (req.query.voicedata) {
-    try {
-      const recognizedText = await recognize(req.query.voicedata);
-      res.json({
-        response: recognizedText,
-        status: 200,
-      });
-    } catch (error) {
-      res.status(500).json({
-        Error: "Error recognizing voice",
-        status: 500,
-        details: error.message,
-      });
-    }
-  } else {
-    res.status(404).json({
-      Error: "Cannot get this route",
-      status: 404,
-    });
+app.post("/api", upload.single("image"), async (req, res) => {
+  try {
+    res.json({ message: `Upload successful: ${req.file}` });
+  } catch (err) {
+    res.json({ error: err });
   }
 });
 
--- src/gemini.js
+++ src/gemini.js
@@ -1,16 +1,45 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleAIFileManager } from "@google/generative-ai/files";
 
 const API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+const fileManager = new GoogleAIFileManager(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-const generate = async (prompt) => {
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  return text;
+const date = new Date();
+
+const generate = async (prompt, image = undefined) => {
+  if (image) {
+    const uploadedImage = await fileManager.uploadFile(image, {
+      mimeType: "image/jpeg",
+      displayName: date.getMilliseconds(),
+    });
+
+    console.log(
+      `Uploaded file ${uploadedImage.file.displayName} as: ${uploadedImage.file.uri}`
+    );
+
+    const result = await model.generateContent([
+      {
+        fileData: {
+          mimeType: uploadedImage.file.mimeType,
+          fileUri: uploadedImage.file.uri,
+        },
+      },
+      { text: prompt },
+    ]);
+
+    const response = await result.response;
+    const text = response.text();
+    return text;
+  } else {
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    return text;
+  }
 };
 
 export default generate;
